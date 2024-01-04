bfc853f16d328af021496bf76192d6e95dcf8c62
AnandC7github <122742595+AnandC7github@users.noreply.github.com>
2024-01-04 09:26:37 +0000

    Created a basic Flask Application

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+  return 'Hello, World!'
+
+
+@app.route('/home')
+def hello_home():
+  return 'Hello, home!'
+
+
+@app.route('/view')
+def hello_view():
+  return 'Hello, view!'
+
+
+#testing
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=8080, debug=True)
+  #print("Hello!")
